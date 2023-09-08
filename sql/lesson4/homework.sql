c08f22ecffedaaee6c6d537347834f67991521b6
DaniilShelofast <shelofast.daniil@gmail.com>
2023-09-08 14:53:08 +0300

    home task sql.

--- sql/lesson4/homework.sql
+++ sql/lesson4/homework.sql
@@ -1,18 +1,18 @@
-  create table Hotel (
+  create table HOTEL (
   ID bigint,
-  CONSTRAINT Hotel_ID PRIMARY KEY (ID),
+  CONSTRAINT HOTEL_ID PRIMARY KEY (ID),
   COUNTRY nvarchar(50) default 'N/A',
   CITY nvarchar(50) not null,
   STREET nvarchar(50),
   CONSTRAINT TEST_COUNTRY check (COUNTRY != 'Romania' and COUNTRY != 'China' and COUNTRY != 'Russia' and COUNTRY != 'Belgium')
   );
 
-  create table orders(
-  ID bigint check (ID < 10000),
-  constraint ORDER_ID primary key(ID),
-  USER_ID bigint unique,
-  ROOM_ID bigint unique,
-  DATE_FROM timestamp,
-  DATE_TO timestamp,
-  MONEY_PAIN double
-  );
+  create table ORDERS (
+    ID bigint check (ID < 10000),
+    constraint ORDER_ID primary key(ID),
+    USER_ID bigint unique,
+    ROOM_ID bigint unique,
+    DATE_FROM timestamp,
+    DATE_TO timestamp,
+    MONEY_PAID decimal (10, 2)
+    );
