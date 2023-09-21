12ee4dab45ecb1832d2c50e4e550e1dc5101f78b
DaniilShelofast <shelofast.daniil@gmail.com>
2023-09-21 18:37:54 +0300

    home task.

--- sql/lesson6/homeworktask2.sql
+++ sql/lesson6/homeworktask2.sql
@@ -1,58 +1,64 @@
 
-create table torum_categories(
-ID bigint,
-constraint categories_pk primary key (ID),
-TITLE varchar(64),
-DESCRIPTION text,
-DATE timestamp,
-IP varchar(20)
+CREATE TABLE torum_categories (
+    ID BIGINT,
+    CONSTRAINT categories_pk PRIMARY KEY (ID),
+    TITLE VARCHAR(64),
+    DESCRIPTION TEXT,
+    DATE TIMESTAMP,
+    IP VARCHAR(20)
 );
 
-create table torum_subcategories(
-ID bigint,
-constraint subcategories_pk primary key (ID),
-ID_CATEGORY bigint,
-constraint ID_CATEGORY foreign key (ID_CATEGORY) references torum_categories(ID),
-TITLE varchar (45),
-DESCRIPTION text,
-DATE timestamp,
-IP varchar(20)
+CREATE TABLE torum_subcategories (
+    ID BIGINT,
+    CONSTRAINT subcategories_pk PRIMARY KEY (ID),
+    ID_CATEGORY BIGINT,
+    CONSTRAINT ID_CATEGORY FOREIGN KEY (ID_CATEGORY)
+        REFERENCES torum_categories (ID),
+    TITLE VARCHAR(45),
+    DESCRIPTION TEXT,
+    DATE TIMESTAMP,
+    IP VARCHAR(20)
 );
 
-create table users(
-ID bigint,
-constraint users_pk primary key (ID),
-NICK varchar (16),
-PASSWORD varchar (128),
-EMAIL varchar(128),
-DATE timestamp,
-KARMA decimal(9,2),
-IP varchar(20)
+CREATE TABLE users (
+    ID BIGINT,
+    CONSTRAINT users_pk PRIMARY KEY (ID),
+    NICK VARCHAR(16),
+    PASSWORD VARCHAR(128),
+    EMAIL VARCHAR(128),
+    DATE TIMESTAMP,
+    KARMA DECIMAL(9 , 2 ),
+    IP VARCHAR(20)
 );
 
-create table torum_polls_options(
-ID bigint,
-constraint torum_polls_options_pk primary key (ID),
-ID_POST bigint,
-constraint ID_POST foreign key (ID_POST) references torum_posts (ID),
-TITLE varchar (64),
-DATE timestamp
+
+CREATE TABLE torum_polls_options (
+    ID BIGINT,
+    CONSTRAINT torum_polls_options_pk PRIMARY KEY (ID),
+    ID_POST BIGINT,
+    CONSTRAINT ID_POST FOREIGN KEY (ID_POST)
+        REFERENCES torum_posts (ID),
+    TITLE VARCHAR(64),
+    DATE TIMESTAMP
 );
 
-create table torum_posts(
-ID bigint,
-constraint torum_posts_pk primary key (ID),
-ID_SUBCATEGORY bigint,
-constraint ID_SUBCATEGORY foreign key (ID_SUBCATEGORY) references torum_subcategories (ID),
-ID_USER bigint,
-constraint ID_USER foreign key (ID_USER) references users(ID),
-PARENT_POST bigint,
-constraint PARENT_POST foreign key (PARENT_POST) references torum_polls_options (ID),
-TITLE varchar (90),
-CONTENT text,
-IS_POLL boolean,
-DATE timestamp,
-IP varchar(20)
+CREATE TABLE torum_posts (
+    ID BIGINT,
+    CONSTRAINT torum_posts_pk PRIMARY KEY (ID),
+    ID_SUBCATEGORY BIGINT,
+    CONSTRAINT ID_SUBCATEGORY FOREIGN KEY (ID_SUBCATEGORY)
+        REFERENCES torum_subcategories (ID),
+    ID_USER BIGINT,
+    CONSTRAINT ID_USER FOREIGN KEY (ID_USER)
+        REFERENCES users (ID),
+    PARENT_POST BIGINT,
+    CONSTRAINT PARENT_POST FOREIGN KEY (PARENT_POST)
+        REFERENCES torum_polls_options (ID),
+    TITLE VARCHAR(90),
+    CONTENT TEXT,
+    IS_POLL BOOLEAN,
+    DATE TIMESTAMP,
+    IP VARCHAR(20)
 );
 
 ----------------------
